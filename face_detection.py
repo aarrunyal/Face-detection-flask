edc571a03486738ca0b22f6a2e2a4437b36711f8
aarrunyal <aarrunyal@gmail.com>
2023-07-31 01:17:54 -0400

    changes

--- face_detection.py
+++ face_detection.py
@@ -11,7 +11,7 @@ video_capture = cv2.VideoCapture(0)
 
 def camera_stream():
     ret, frame = video_capture.read()
-
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(

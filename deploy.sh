b030ae9d06231be4eb3b9218f30faff6da446711
aarrunyal <aarrunyal@gmail.com>
2023-07-31 00:02:59 -0400

    changes

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ echo "******************New env created ******************"
 
 
 echo "******************Pip package installation ongoing******************"
-pip3 install flask
 pip3 install wheel
 pip3 install gunicorn
 pip3 install opencv-python
+pip3 install flask
 echo "******************Pip package installation ongoing******************"
 
 if (systemctl -q is-active face-detection.service)
